c947991aba04051d414780af427518788f9839e0
Melvin Carvalho <melvincarvalho@gmail.com>
2018-10-29 21:12:53 +0100

    getting started with add script, addresses #19

--- bin/addbookmark.sh
+++ bin/addbookmark.sh
@@ -16,6 +16,22 @@ MAKER="$3"
 #        <#0.01928626921678589
 #
 
-# TODO create heredoc with correct terms
+ID="#0.01928626921678589"
+CREATED="2018-10-17T17:30:02.020Z"
+TITLE="Apple Launches Portal for U.S. Users to Download Their Data"
+RECALLS="https://www.bloomberg.com/news/articles/2018-10-17/apple-launches-portal-for-u-s-users-to-download-their-data"
+MAKER="https://melvincarvalho.com/#me"
+URI=$1
 
-curl -X PATCH -H "Content-Type:application/sparql-update" "$URI" -d  "INSERT DATA { <> a <http://www.w3.org/2002/01/bookmark#Bookmark> . } "
+read -r -d '' BODY <<"EOF"
+INSERT DATA {
+  <> <http://purl.org/dc/terms/references> <$ID> .
+  <$ID> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/01/bookmark#Bookmark> .
+  <$ID> <http://purl.org/dc/terms/created> "$CREATED"^^XML:dateTime .
+  <$ID> <http://purl.org/dc/terms/title> """$TITLE""" .
+  <$ID> <http://www.w3.org/2002/01/bookmark/recalls> """$RECALLS""" .
+  <$ID> <http://xmlns.com/foaf/0.1/maker> """$MAKER""" .
+}
+EOF
+
+echo curl -X PATCH -H "Content-Type:application/sparql-update" "$URI" -d  "$BODY"
