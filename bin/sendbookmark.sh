dd45548435a202a76d83215909bfb7ef7a483513
Melvin Carvalho <melvincarvalho@gmail.com>
2019-01-22 14:06:51 +0100

    add sendbookmark for inboxes

--- bin/sendbookmark.sh
+++ bin/sendbookmark.sh
@@ -35,16 +35,14 @@ fi
 # DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") ;  bin/addbookmark.sh  https://bookmarks.inrupt.net/inbox/b.ttl "ifps:foo" "https://melvincarvalho.com/#me" "IPFS link" "0.${RANDOM}" $DATE
 
 read -r -d '' BODY <<EOF
-INSERT DATA {
   <> <http://purl.org/dc/terms/references> <$ID> .
   <$ID> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/01/bookmark#Bookmark> .
   <$ID> <http://purl.org/dc/terms/created> "$CREATED"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
   <$ID> <http://purl.org/dc/terms/title> """$TITLE""" .
   <$ID> <http://www.w3.org/2002/01/bookmark#recalls> <$RECALLS> .
   <$ID> <http://xmlns.com/foaf/0.1/maker> """$MAKER""" .
-}
 EOF
 
 # @TODO : get authn params
-echo curl -k -X POST -H "Content-Type: text/turtle" "$URI" -d  "$BODY" 2>&1
-curl -k -X POST -H "Content-Type: text/turtle" "$URI" -d  "$BODY"
+echo curl -k -X POST -H "Content-Type: text/turtle" "$URI" --data  "$BODY" 2>&1
+curl -k -X POST -H "Content-Type: text/turtle" "$URI" --data  "$BODY"
