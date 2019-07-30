ae784eb09875339988ac91bf1357db35c4d49740
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-30 21:55:56 +0200

    standardjs

--- bin/reddit.js
+++ bin/reddit.js
@@ -2,34 +2,32 @@ let fetch = require('node-fetch')
 let sub = process.argv[2] || 'EarthPorn'
 let limit = '1000'
 
-
-fetch('https://www.reddit.com/r/'+ sub +'/.json?limit=' + limit)
+fetch('https://www.reddit.com/r/' + sub + '/.json?limit=' + limit)
   .then(
-    function(response) {
+    function (response) {
       if (response.status !== 200) {
         console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
+          response.status)
+        return
       }
 
-      response.json().then(function(data) {
-
+      response.json().then(function (data) {
         var children = data.data.children
 
-          children.forEach( (k, v) => {
+        children.forEach((k, v) => {
           let title = k.data.title
           let recalls = k.data.url
           let created = k.data.created
           let offset = 8 * 3600
           time = new Date((created - offset) * 1000).toISOString()
-          //console.log(k.data)
+          // console.log(k.data)
 
           let user = k.data.author
           let userURI = 'https://www.reddit.com/user/' + user + '#this'
-          //const time = new Date().toISOString();
+          // const time = new Date().toISOString();
 
-          const ms = Math.floor(Math.random() * 1000);
-          const uri = time.substring(0,4) + time.substring(5,7) + time.substring(8,10) + time.substring(11,13) + time.substring(14,16) + time.substring (17,19) + "." + ms;
+          const ms = Math.floor(Math.random() * 1000)
+          const uri = time.substring(0, 4) + time.substring(5, 7) + time.substring(8, 10) + time.substring(11, 13) + time.substring(14, 16) + time.substring(17, 19) + '.' + ms
           console.log(`
           <> <http://purl.org/dc/terms/references> <#${uri}> .\n
           \n
@@ -39,13 +37,11 @@ fetch('https://www.reddit.com/r/'+ sub +'/.json?limit=' + limit)
           <#${uri}> <http://purl.org/dc/terms/title> """ ${title} """ .\n
           <#${uri}> <http://www.w3.org/2002/01/bookmark#recalls> <${recalls}> .\n
           <#${uri}> <http://xmlns.com/foaf/0.1/maker> <${userURI}> .\n
-          `);
-        
+          `)
         })
-        
-      });
+      })
     }
   )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+  .catch(function (err) {
+    console.log('Fetch Error :-S', err)
+  })
