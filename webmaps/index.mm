44049438c908b9efb1ec167541e4c677476dc97f
Melvin Carvalho <melvincarvalho@gmail.com>
2020-01-13 14:15:39 +0100

    improve webmap

--- webmaps/index.mm
+++ webmaps/index.mm
@@ -64,7 +64,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="13" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="Demo" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1672887815" CREATED="1578920361269" MODIFIED="1578920558012" LINK="https://markbook.org/">
 <icon BUILTIN="wizard"/>
 <edge COLOR="#007c00"/>
@@ -87,23 +87,52 @@
 <icon BUILTIN="revision"/>
 <edge COLOR="#00007c"/>
 <font BOLD="true"/>
-<node TEXT="            &lt;&gt; &lt;http://purl.org/dc/terms/references&gt; &lt;${id}&gt; .&#xa;&#xa;            &lt;${id}&gt; a &lt;http://www.w3.org/2002/01/bookmark#Bookmark&gt; ;&#xa;            &lt;http://purl.org/dc/terms/title&gt;   &quot;&quot;&quot;${title}&quot;&quot;&quot; ;&#xa;            &lt;http://xmlns.com/foaf/0.1/maker&gt;   &lt;${template.profile.webId}&gt; ;&#xa;            &lt;http://purl.org/dc/terms/created&gt;  &quot;${d}&quot;^^&lt;http://www.w3.org/2001/XMLSchema#dateTime&gt;;&#xa;            &lt;http://www.w3.org/2002/01/bookmark#recalls&gt; &lt;${uri}&gt; ." ID="ID_1310058374" CREATED="1578919756017" MODIFIED="1578919892670"/>
-<node TEXT="Vocabs" ID="ID_129926507" CREATED="1578920490373" MODIFIED="1578920492221">
-<node TEXT="FOAF" ID="ID_500156053" CREATED="1578920493112" MODIFIED="1578920615264" LINK="http://xmlns.com/foaf/0.1/"/>
-<node TEXT="DCT" ID="ID_599327468" CREATED="1578920497327" MODIFIED="1578920629700" LINK="http://purl.org/dc/terms/"/>
-<node TEXT="BOOKMARK" ID="ID_32599561" CREATED="1578920501068" MODIFIED="1578920641110" LINK="http://www.w3.org/2002/01/bookmark#"/>
+<node TEXT="            &lt;&gt; &lt;http://purl.org/dc/terms/references&gt; &lt;${id}&gt; .&#xa;&#xa;            &lt;${id}&gt; a &lt;http://www.w3.org/2002/01/bookmark#Bookmark&gt; ;&#xa;            &lt;http://purl.org/dc/terms/title&gt;   &quot;&quot;&quot;${title}&quot;&quot;&quot; ;&#xa;            &lt;http://xmlns.com/foaf/0.1/maker&gt;   &lt;${template.profile.webId}&gt; ;&#xa;            &lt;http://purl.org/dc/terms/created&gt;  &quot;${d}&quot;^^&lt;http://www.w3.org/2001/XMLSchema#dateTime&gt;;&#xa;            &lt;http://www.w3.org/2002/01/bookmark#recalls&gt; &lt;${uri}&gt; ." ID="ID_1310058374" CREATED="1578919756017" MODIFIED="1578920790103">
+<cloud COLOR="#ffffcc" SHAPE="ROUND_RECT"/>
 </node>
 </node>
-<node TEXT="links" POSITION="left" ID="ID_808607355" CREATED="1570968728188" MODIFIED="1570968728190">
+<node TEXT="Structure" POSITION="right" ID="ID_1985440428" CREATED="1578920805028" MODIFIED="1578920814684">
+<icon BUILTIN="xmag"/>
+<edge COLOR="#007c7c"/>
+<font BOLD="true"/>
+<node TEXT="WebID is obtained from Login" ID="ID_690330746" CREATED="1578920816388" MODIFIED="1578920869368"/>
+<node TEXT="publicTypeIndex obtained from WebID" ID="ID_1974744289" CREATED="1578920869726" MODIFIED="1578920885578"/>
+<node TEXT="bookmark file put in publicTypeIndex" ID="ID_50156365" CREATED="1578920886750" MODIFIED="1578920895537">
+<node TEXT="public/bookmarks.ttl" ID="ID_1011719771" CREATED="1578921184063" MODIFIED="1578921308829">
+<node TEXT="default" ID="ID_248766247" CREATED="1578921302366" MODIFIED="1578921304033"/>
+</node>
+</node>
+</node>
+<node TEXT="links" POSITION="left" ID="ID_808607355" CREATED="1570968728188" MODIFIED="1578920755487">
 <icon BUILTIN="attach"/>
 <edge COLOR="#00007c"/>
+<font BOLD="true"/>
 <node TEXT="github" ID="ID_885967166" CREATED="1578918188827" MODIFIED="1578918193657" LINK="https://github.com/mark-book/markbook"/>
 <node TEXT="issues" ID="ID_780116375" CREATED="1578918451453" MODIFIED="1578918459763" LINK="https://github.com/mark-book/markbook/issues"/>
 </node>
-<node TEXT="docs" POSITION="left" ID="ID_1100215200" CREATED="1570969039178" MODIFIED="1570980265221">
+<node TEXT="docs" POSITION="left" ID="ID_1100215200" CREATED="1570969039178" MODIFIED="1578920756492">
 <icon BUILTIN="folder"/>
 <edge COLOR="#ff00ff"/>
+<font BOLD="true"/>
 <node TEXT="README" ID="ID_387137590" CREATED="1578918236074" MODIFIED="1578920105050" LINK="https://github.com/mark-book/markbook#markbook"/>
+</node>
+<node TEXT="libs" POSITION="left" ID="ID_1286983084" CREATED="1578921023960" MODIFIED="1578921136898">
+<icon BUILTIN="16x16-free-application-icons/png/16x16/Left-right"/>
+<edge COLOR="#7c7c00"/>
+<font BOLD="true"/>
+<node TEXT="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js" ID="ID_851897190" CREATED="1578921033498" MODIFIED="1578921077420" LINK="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"/>
+<node TEXT="https://solid.github.io/solid-auth-client/dist/solid-auth-client.bundle.js" ID="ID_1873299396" CREATED="1578921033498" MODIFIED="1578921084978" LINK="https://solid.github.io/solid-auth-client/dist/solid-auth-client.bundle.js"/>
+<node TEXT="https://melvincarvalho.github.io/helloworld/scripts/rdflib.min.js" ID="ID_1373373760" CREATED="1578921033501" MODIFIED="1578921091190" LINK="https://melvincarvalho.github.io/helloworld/scripts/rdflib.min.js"/>
+</node>
+<node TEXT="Vocabs" POSITION="left" ID="ID_129926507" CREATED="1578920490373" MODIFIED="1578921168867">
+<icon BUILTIN="pencil"/>
+<edge COLOR="#ff0000"/>
+<font BOLD="true"/>
+<node TEXT="FOAF" ID="ID_500156053" CREATED="1578920493112" MODIFIED="1578920615264" LINK="http://xmlns.com/foaf/0.1/"/>
+<node TEXT="DCT" ID="ID_599327468" CREATED="1578920497327" MODIFIED="1578920629700" LINK="http://purl.org/dc/terms/"/>
+<node TEXT="BOOKMARK" ID="ID_32599561" CREATED="1578920501068" MODIFIED="1578920641110" LINK="http://www.w3.org/2002/01/bookmark#"/>
+<node TEXT="RDF" ID="ID_1370597730" CREATED="1578920923416" MODIFIED="1578920949302" LINK="http://www.w3.org/1999/02/22-rdf-syntax-ns#"/>
+<node TEXT="SOLID" ID="ID_134386448" CREATED="1578920925271" MODIFIED="1578920938409" LINK="http://www.w3.org/ns/solid/terms#"/>
 </node>
 </node>
 </map>
