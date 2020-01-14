8be601befc88c4477d6e95281f85515e05000cf8
Melvin Carvalho <melvincarvalho@gmail.com>
2020-01-14 17:19:16 +0100

    add user guide

--- webmaps/index.mm
+++ webmaps/index.mm
@@ -64,16 +64,24 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="13" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="Demo" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1672887815" CREATED="1578920361269" MODIFIED="1578920558012" LINK="https://markbook.org/">
+<hook NAME="AutomaticEdgeColor" COUNTER="15" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Demo" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1672887815" CREATED="1578920361269" MODIFIED="1579018458540" LINK="https://markbook.org/">
 <icon BUILTIN="wizard"/>
 <edge COLOR="#007c00"/>
-</node>
-<node TEXT="App" POSITION="right" ID="ID_120250381" CREATED="1570979967089" MODIFIED="1578920390307">
-<icon BUILTIN="list"/>
-<edge COLOR="#00ff00"/>
 <font BOLD="true"/>
-<node TEXT="Markbook is a bookmarking app for the solid platform" ID="ID_495239312" CREATED="1578918204474" MODIFIED="1578918212633"/>
+<node TEXT="User Guide" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1666578229" CREATED="1579018417540" MODIFIED="1579018450721" LINK="user/guide/index.mm">
+<icon BUILTIN="group"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Markbook" POSITION="right" ID="ID_120250381" CREATED="1570979967089" MODIFIED="1579018591702" COLOR="#336600">
+<icon BUILTIN="list"/>
+<font BOLD="true"/>
+<edge COLOR="#00ff00"/>
+<node TEXT="a bookmarking app for the solid platform" ID="ID_495239312" CREATED="1578918204474" MODIFIED="1579018609617" COLOR="#339900">
+<font BOLD="true"/>
+</node>
 </node>
 <node TEXT="Code" POSITION="right" ID="ID_383868314" CREATED="1578918629879" MODIFIED="1578920406958">
 <icon BUILTIN="edit"/>
@@ -87,7 +95,7 @@
 <icon BUILTIN="revision"/>
 <edge COLOR="#00007c"/>
 <font BOLD="true"/>
-<node TEXT="            &lt;&gt; &lt;http://purl.org/dc/terms/references&gt; &lt;${id}&gt; .&#xa;&#xa;            &lt;${id}&gt; a &lt;http://www.w3.org/2002/01/bookmark#Bookmark&gt; ;&#xa;            &lt;http://purl.org/dc/terms/title&gt;   &quot;&quot;&quot;${title}&quot;&quot;&quot; ;&#xa;            &lt;http://xmlns.com/foaf/0.1/maker&gt;   &lt;${template.profile.webId}&gt; ;&#xa;            &lt;http://purl.org/dc/terms/created&gt;  &quot;${d}&quot;^^&lt;http://www.w3.org/2001/XMLSchema#dateTime&gt;;&#xa;            &lt;http://www.w3.org/2002/01/bookmark#recalls&gt; &lt;${uri}&gt; ." ID="ID_1310058374" CREATED="1578919756017" MODIFIED="1578920790103">
+<node TEXT="            &lt;&gt; &lt;http://purl.org/dc/terms/references&gt; &lt;${id}&gt; .&#xa;&#xa;            &lt;${id}&gt; a &lt;http://www.w3.org/2002/01/bookmark#Bookmark&gt; ;&#xa;            &lt;http://purl.org/dc/terms/title&gt;   &quot;&quot;&quot;${title}&quot;&quot;&quot; ;&#xa;            &lt;http://xmlns.com/foaf/0.1/maker&gt;   &lt;${template.profile.webId}&gt; ;&#xa;            &lt;http://purl.org/dc/terms/created&gt;  &quot;${d}&quot;^^&lt;http://www.w3.org/2001/XMLSchema#dateTime&gt;;&#xa;            &lt;http://www.w3.org/2002/01/bookmark#recalls&gt; &lt;${uri}&gt; ." ID="ID_1310058374" CREATED="1578919756017" MODIFIED="1579018245469" BORDER_WIDTH="0.6 px" BORDER_DASH="SOLID">
 <cloud COLOR="#ffffcc" SHAPE="ROUND_RECT"/>
 </node>
 </node>
---
+++ webmaps/user/guide/index.mm
@@ -0,0 +1,84 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="User Guide" FOLDED="false" ID="ID_439809472" CREATED="1579018368157" MODIFIED="1579018641811" LINK="../../index.mm" STYLE="oval">
+<icon BUILTIN="group"/>
+<edge STYLE="bezier" COLOR="#00ff00" WIDTH="thin" DASH="SOLID"/>
+<font SIZE="18" BOLD="false"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="19" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="quick start" POSITION="right" ID="ID_1679770593" CREATED="1579018683706" MODIFIED="1579018715387">
+<icon BUILTIN="launch"/>
+<edge COLOR="#00ffff"/>
+<font BOLD="true"/>
+<node TEXT="Register: Register with a solid pod if you have not already" ID="ID_863429899" CREATED="1579018683706" MODIFIED="1579018704013">
+<icon BUILTIN="full-1"/>
+</node>
+<node TEXT="Login: Login to your solid pod" ID="ID_361241044" CREATED="1579018683707" MODIFIED="1579018705484">
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="Create: Clicking on create bookmark - adds a bookmark to your pod" ID="ID_1528346128" CREATED="1579018683710" MODIFIED="1579018706466">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+</map>
