d9190e8e2b86682b2bc1e668163d6051da6876f8
Melvin Carvalho <melvincarvalho@gmail.com>
2020-01-20 15:01:27 +0100

    test for maker

--- v2/src/components/Bookmark.jsx
+++ v2/src/components/Bookmark.jsx
@@ -6,85 +6,141 @@ UI.store = $rdf.graph()
 UI.fetcher = new $rdf.Fetcher(UI.store)
 UI.updater = new $rdf.UpdateManager(UI.store)
 
-
-function BookmarkItem(props) {
+function BookmarkItem (props) {
   const AUDIO_EXTENSIONS = /\.(m4a|mp4a|mpga|mp2|mp2a|mp3|m2a|m3a|wav|weba|aac|oga|spx)($|\?)/i
   const VIDEO_EXTENSIONS = /\.(mp4|og[gv]|webm|mov|m4v|mkv)($|\?)/i
   const IMAGE_EXTENSIONS = /\.(png|gif|bmp|svg|jpeg|jpg)($|\?)/i
   const URL = /http/i
 
-
   if (props.recalls.match(IMAGE_EXTENSIONS)) {
     return (
-      <div className="box">
+      <div className='box'>
         <table>
           <tbody>
             <tr>
               <td>{props.id + 1}.&nbsp;</td>
-              <td><a target="_blank" href={props.recalls}>{props.title}</a> <a target="_blank" href={props.subject}><img height="10" width="10" src="./image/External.svg" /></a></td>
-            </tr>
-            <tr>
-            <td></td>
-              <td><sup>{moment.utc(props.created).fromNow()} by <a href={props.maker} target="_blank" style={{ color : 'inherit' }}>{props.maker}</a></sup></td>
-            </tr>
-            <tr>
-            <td></td>
-              <td><img src={props.recalls} /></td>
-
-            </tr>
-
-          </tbody>
-        </table>
-
-        </div>)
-  } else if (props.recalls.match(VIDEO_EXTENSIONS)) {
-    return (
-      <div>{props.id + 1}. <video controls autoplay='true' loop src={props.recalls} /></div>)
-  } else if (props.recalls.match(AUDIO_EXTENSIONS)) {
-    return (
-      <div>{props.id + 1}. <video controls autoplay='true' loop src={props.recalls} /></div>)
-    } else if (props.recalls.match(URL)) {
-      return (
-        <div className="box">
-          <table>
-            <tbody>
-              <tr>
-                <td>{props.id + 1}.&nbsp;</td>
-                <td><a target="_blank" href={props.recalls}>{props.title}</a> <a target="_blank" href={props.subject}><img height="10" width="10" src="./image/External.svg" /></a></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td><sup>{moment.utc(props.created).fromNow()} by <a href={props.maker} target="_blank" style={{ color : 'inherit' }}>{props.maker}</a></sup></td>
-              </tr>
-  
-            </tbody>
-          </table>
-  
-        </div>
-      )
-      } else {
-    return (
-      <div className="box">
-        <table>
-          <tbody>
-            <tr>
-              <td>{props.id + 1}.&nbsp;</td>
-              <td><a target="_blank" href={props.recalls}>{props.title}</a> <a target="_blank" href={props.subject}><img height="10" width="10" src="./image/External.svg" /></a></td>
+              <td>
+                <a target='_blank' href={props.recalls}>
+                  {props.title}
+                </a>{' '}
+                <a target='_blank' href={props.subject}>
+                  <img height='10' width='10' src='./image/External.svg' />
+                </a>
+              </td>
             </tr>
             <tr>
               <td></td>
-              <td><sup>{moment.utc(props.created).fromNow()} by <a href={props.maker} target="_blank" style={{ color : 'inherit' }}>{props.maker}</a></sup></td>
+              <td>
+                <sup>
+                  {moment.utc(props.created).fromNow()} by{' '}
+                  <a
+                    href={props.maker}
+                    target='_blank'
+                    style={{ color: 'inherit' }}
+                  >
+                    {props.maker}
+                  </a>
+                </sup>
+              </td>
             </tr>
-
+            <tr>
+              <td></td>
+              <td>
+                <img src={props.recalls} />
+              </td>
+            </tr>
           </tbody>
         </table>
-
+      </div>
+    )
+  } else if (props.recalls.match(VIDEO_EXTENSIONS)) {
+    return (
+      <div>
+        {props.id + 1}.{' '}
+        <video controls autoplay='true' loop src={props.recalls} />
+      </div>
+    )
+  } else if (props.recalls.match(AUDIO_EXTENSIONS)) {
+    return (
+      <div>
+        {props.id + 1}.{' '}
+        <video controls autoplay='true' loop src={props.recalls} />
+      </div>
+    )
+  } else if (props.recalls.match(URL)) {
+    return (
+      <div className='box'>
+        <table>
+          <tbody>
+            <tr>
+              <td>{props.id + 1}.&nbsp;</td>
+              <td>
+                <a target='_blank' href={props.recalls}>
+                  {props.title}
+                </a>{' '}
+                <a target='_blank' href={props.subject}>
+                  <img height='10' width='10' src='./image/External.svg' />
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>
+                <sup>
+                  {moment.utc(props.created).fromNow()} by{' '}
+                  <a
+                    href={props.maker}
+                    target='_blank'
+                    style={{ color: 'inherit' }}
+                  >
+                    {props.maker}
+                  </a>
+                </sup>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  } else {
+    return (
+      <div className='box'>
+        <table>
+          <tbody>
+            <tr>
+              <td>{props.id + 1}.&nbsp;</td>
+              <td>
+                <a target='_blank' href={props.recalls}>
+                  {props.title}
+                </a>{' '}
+                <a target='_blank' href={props.subject}>
+                  <img height='10' width='10' src='./image/External.svg' />
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>
+                <sup>
+                  {moment.utc(props.created).fromNow()} by{' '}
+                  <a
+                    href={props.maker}
+                    target='_blank'
+                    style={{ color: 'inherit' }}
+                  >
+                    {props.maker}
+                  </a>
+                </sup>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
 }
 
-function getTypeFromSubject(subject) {
+function getTypeFromSubject (subject) {
   let s = UI.store.sym(subject)
   let p = UI.store.sym('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
   let o = null
@@ -97,7 +153,7 @@ function getTypeFromSubject(subject) {
   }
 }
 
-function getBookmarkFromSubject(subject) {
+function getBookmarkFromSubject (subject) {
   let s = UI.store.sym(subject)
   let p = UI.store.sym('http://www.w3.org/2002/01/bookmark#recalls')
   let o = null
@@ -119,50 +175,65 @@ function getBookmarkFromSubject(subject) {
   w = UI.store.sym(subject.split('#')[0])
   let created = UI.store.any(s, p, o, w)
 
-  let bookmark = { 'recalls': recalls.value, 'title': title.value, 'maker' : maker.value, 'created' : created.value, 'subject' : subject }
+  let bookmark = {
+    recalls: recalls.value,
+    title: title.value,
+    created: created.value,
+    subject: subject
+  }
+  if (maker) {
+    bookmark.maker = maker.value
+  }
   return bookmark
 }
 
 class Bookmark extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     let media = this.isMedia(props.subject)
-    this.state = { 'media': media, 'subject': props.subject, 'title': '', bookmark: [{ 'recalls': '', 'title': '' }] }
+    this.state = {
+      media: media,
+      subject: props.subject,
+      title: '',
+      bookmark: [{ recalls: '', title: '' }]
+    }
   }
 
-  fetchBookmark(subject, force) {
-    force = !! force
+  fetchBookmark (subject, force) {
+    force = !!force
     console.log('fetch bookmark', subject, 'force', force)
     // hack to force fetcher
     UI.store = $rdf.graph()
-    UI.fetcher = new $rdf.Fetcher(UI.store)    
-    UI.fetcher.load(subject, {force : true} ).then(response => {
-      var type = getTypeFromSubject(subject)
-      var bm = []
-      console.log('type', type);
+    UI.fetcher = new $rdf.Fetcher(UI.store)
+    UI.fetcher.load(subject, { force: true }).then(
+      response => {
+        var type = getTypeFromSubject(subject)
+        var bm = []
+        console.log('type', type)
 
-      if (!type) {
-        let s = UI.store.sym(subject)
-        let p = UI.store.sym('http://purl.org/dc/terms/references')
-        let subjects = UI.store.statementsMatching(s, p)
-        for (let subject of subjects) {
-          bm.push(getBookmarkFromSubject(subject.object.value))
+        if (!type) {
+          let s = UI.store.sym(subject)
+          let p = UI.store.sym('http://purl.org/dc/terms/references')
+          let subjects = UI.store.statementsMatching(s, p)
+          for (let subject of subjects) {
+            bm.push(getBookmarkFromSubject(subject.object.value))
+          }
+
+          bm = bm.sort(function (a, b) {
+            return b.created < a.created ? -1 : b.created > a.created ? 1 : 0
+          })
+
+          this.setState({ bookmark: bm })
+        } else {
+          bm.push(getBookmarkFromSubject(subject))
+
+          this.setState({ bookmark: bm })
         }
-
-        bm = bm.sort( function(a,b) { 
-          return (b.created < a.created) ? -1 : ((b.created > a.created) ? 1 : 0);
-        } )
-
-        this.setState({ 'bookmark': bm })
-
-      } else {
-        bm.push(getBookmarkFromSubject(subject))
-
-        this.setState({ 'bookmark': bm })
+      },
+      err => {
+        console.log(err)
       }
-    }, err => {
-      console.log(err)
-    })
+    )
   }
 
   getUpdatesVia (doc) {
@@ -172,13 +243,13 @@ class Bookmark extends React.Component {
     return linkHeaders[0].trim()
   }
 
-  setRefreshHandler(subject, handler) {
+  setRefreshHandler (subject, handler) {
     var self = this
     var wss = this.getUpdatesVia(subject)
     console.log('wss', wss, 'subject', subject)
     let w = new WebSocket(wss)
     w.onmessage = function (m) {
-      let data = m.data      
+      let data = m.data
       console.log('data', data)
       if (data.match(/pub .*/)) {
         console.log('refresh')
@@ -188,14 +259,14 @@ class Bookmark extends React.Component {
     }
     w.onopen = function () {
       w.send('sub ' + subject)
-    }  
+    }
   }
 
-  refresh() {
+  refresh () {
     this.fetchBookmark(this.state.subject, true)
   }
 
-  componentDidMount() {
+  componentDidMount () {
     let subject = this.state.subject
     if (this.isMedia(subject) === false) {
       this.fetchBookmark(subject)
@@ -203,22 +274,22 @@ class Bookmark extends React.Component {
     if (subject) {
       console.log('init subject', subject)
       setTimeout(() => {
-        this.setRefreshHandler (subject, this.refresh)         
-      }, 1000);
+        this.setRefreshHandler(subject, this.refresh)
+      }, 1000)
     }
   }
 
-  componentWillReceiveProps(props) {
+  componentWillReceiveProps (props) {
     let subject = props.subject
     if (this.isMedia(subject) === false) {
       this.fetchBookmark(subject)
     }
   }
 
-  isMedia(subject) {
+  isMedia (subject) {
     // TODO better test for linked data
     let isMedia = false
-    if (subject.match(/.ttl/) || subject.match(/\/[^.]*$/) ) {
+    if (subject.match(/.ttl/) || subject.match(/\/[^.]*$/)) {
       isMedia = false
     } else {
       isMedia = true
@@ -226,28 +297,30 @@ class Bookmark extends React.Component {
     return isMedia
   }
 
-  render() {
+  render () {
     let med = this.isMedia(this.props.subject)
-    console.log('subject', this.props.subject, med);
+    console.log('subject', this.props.subject, med)
 
     if (med === true) {
-      return (
-        <Media href={this.props.subject} />
-      )
+      return <Media href={this.props.subject} />
     } else {
-      const listItems = this.state.bookmark.map((b, i) =>
+      const listItems = this.state.bookmark.map((b, i) => (
         <div>
-          <BookmarkItem key={i} id={i} recalls={b.recalls} title={b.title} maker={b.maker} created={b.created} subject={b.subject}/>
+          <BookmarkItem
+            key={i}
+            id={i}
+            recalls={b.recalls}
+            title={b.title}
+            maker={b.maker}
+            created={b.created}
+            subject={b.subject}
+          />
         </div>
-      )
+      ))
 
-      return (
-        <div>{listItems}</div>
-      )
+      return <div>{listItems}</div>
     }
   }
 }
-
-
 
 //REMOVE export default Bookmark
